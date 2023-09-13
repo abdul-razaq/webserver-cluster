808b00a55e22c7eeff833a3c47fa6630a9da99b5
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2023-09-13 15:46:05 +0100

    created cloudwatch policies to be attached to users

--- landing_zone/iam_user/outputs.tf
+++ landing_zone/iam_user/outputs.tf
@@ -2,3 +2,8 @@ output "iam_users_arns" {
   description = "The ARN of the created IAM users"
   value = aws_iam_user.iam_user.arn
 }
+
+output "iam_user_cloudwatch_policy_arn" {
+  description = "Prints the ARN of the cloudwatch IAM policy that got applied to the IAM user. i.e which of the policies got applied to the IAM user, either the read-only version of the policy or the full-access version of the policy"
+  value = one(concat(aws_iam_user_policy_attachment.iam_user_cloudwatch_read-only_access[*].policy_arn, aws_iam_user_policy_attachment.iam_user_cloudwatch_full_access[*].policy_arn))
+}
--- landing_zone/iam_user/variables.tf
+++ landing_zone/iam_user/variables.tf
@@ -2,3 +2,9 @@ variable "iam_user" {
   description = "The IAM user to create"
   type = string
 }
+
+variable "give_user_cloudwatch_full_access" {
+  description = "whether to give the user full cloudwatch access or just read-only cloudwatch access"
+  type = bool
+  default = false  
+}
